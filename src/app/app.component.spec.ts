a9662985fe9099fb7345203eb36003b2f8bd228e
David Moore <dmoore@avoka.com>
2020-04-01 11:43:44 +1100

    Upgrade to Angular 9.1 and add destroy test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,29 +16,14 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  /*it(`should have as title 'test-unit-angular'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('test-unit-angular');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('test-unit-angular app is running!');
-  });*/
-
-  it('should render title', () => {
+  it('should call ngOnDestroy once', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const component = fixture.componentInstance;
 
-    // @ts-ignore
-    spyOn(component, 'ngOnDestroy').and.callThrough();
+    spyOn(component, 'ngOnDestroy');
 
     fixture.destroy();
 
-    // @ts-ignore
     expect(component.ngOnDestroy).toHaveBeenCalledTimes(1);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,6 @@ export class AppComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
+    console.log('destroyed')
   }
 }
